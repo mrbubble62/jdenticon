114d8cbffcad8f1aeb6a66716de5442086ce4da1
mrbubble62 <martin@mrbubble.net>
2019-07-26 15:21:07 -0400

    Increased to 6x6 grid
    increase entropy

--- src/renderer/iconGenerator.js
+++ src/renderer/iconGenerator.js
@@ -27,11 +27,11 @@ function iconGenerator(renderer, hash, x, y, size, config) {
     var graphics = new Graphics(renderer);
     
     // Calculate cell size and ensure it is an integer
-    var cell = 0 | (size / 4);
+    var cell = 0 | (size / 8);
     
     // Since the cell size is integer based, the actual icon will be slightly smaller than specified => center icon
-    x += 0 | (padding + size / 2 - cell * 2);
-    y += 0 | (padding + size / 2 - cell * 2);
+    x += 0 | (padding + size / 4 - cell * 2);
+    y += 0 | (padding + size / 4 - cell * 2);
 
     function renderShape(colorIndex, shapes, index, rotationIndex, positions) {
         var r = rotationIndex ? parseHex(hash, rotationIndex, 1) : 0,
@@ -78,12 +78,16 @@ function iconGenerator(renderer, hash, x, y, size, config) {
     }
 
     // ACTUAL RENDERING
-    // Sides
-    renderShape(0, shapes.outer, 2, 3, [[1, 0], [2, 0], [2, 3], [1, 3], [0, 1], [3, 1], [3, 2], [0, 2]]);
-    // Corners
-    renderShape(1, shapes.outer, 4, 5, [[0, 0], [3, 0], [3, 3], [0, 3]]);
     // Center
-    renderShape(2, shapes.center, 1, null, [[1, 1], [2, 1], [2, 2], [1, 2]]);
+    renderShape(0, shapes.center, 1, null, [[2, 2], [3, 2], [3, 3], [2, 3]]);
+    // Corners
+    renderShape(1, shapes.outer, 2, 12, [[0, 0], [5, 0], [5,5], [0, 5]]);
+    // Sides
+    renderShape(2, shapes.outer, 3, 16, [[1, 0], [4, 0], [4, 5], [1, 5], [0, 1], [5, 1], [5, 4], [0, 4]]);		 
+    renderShape(3, shapes.outer, 4, 20, [[2, 0], [3, 0], [3, 5], [2, 5], [0, 2], [5, 2], [5, 3], [0, 3]]);	
+    // inner
+    renderShape(4, shapes.outer, 5, 24, [[1, 1], [4, 1], [4,4], [1, 4]]);
+    renderShape(1, shapes.outer, 6, 28, [[2, 1], [3, 1], [3, 4], [2, 4], [1, 2], [4, 2], [4, 3], [1, 3]]);
     
     renderer.finish();
 };
